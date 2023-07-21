7624311e076c72f1877fadcff701f2ae3500c5dc
EzraMachoka <ezra.machoka@student.moringaschool.com>
2023-07-21 13:02:13 +0300

    final commit

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ function App() {
   const appClass = isDarkMode ? "App dark" : "App light";
 
   // replace 'false' with a state variable that can be toggled between true and false
-  // this will be used for the Dark Mode Toggle feature
+  // this will be used for the Dark Mode Toggle
 
   return (
     <div className={appClass}>
